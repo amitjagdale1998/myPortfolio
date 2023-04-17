53c21188898021088c27770d4dfa5d736dc8759f
amitjagdale1998 <amitjagdale1998@gmail.com>
2023-04-17 10:57:10 +0530

    commit five

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -22,39 +22,92 @@ var skip_count = 0;
 var skip_delay = 15;
 var speed = 80;
 
+// var wordflick = function () {
+//   setInterval(function () {
+//     if (forwards) {
+//       if (offset >= words[i].length) {
+//         ++skip_count;
+//         if (skip_count == skip_delay) {
+//           forwards = false;
+//           skip_count = 0;
+//         }
+//       }
+//     }
+//     else {
+//       if (offset == 0) {
+//         forwards = true;
+//         i++;
+//         offset = 0;
+//         if (i >= len) {
+//           i = 0;
+//         }
+//       }
+//     }
+//     part = words[i].substring(0, offset);
+//     if (skip_count == 0) {
+//       if (forwards) {
+//         offset++;
+//       }
+//       else {
+//         offset--;
+//       }
+//     }
+//     document.querySelector('.word').textContent = part;
+//   }, speed);
+// };
+var words = ['MERNstack Dev','FullStack Dev','Web Dev','Web Designer'];
+var part;
+var i = 0;
+var offset = 0;
+var len = words.length;
+var forwards = true;
+var skip_count = 0;
+var skip_delay = 15;
+var speed = 80;
+
 var wordflick = function () {
-  setInterval(function () {
-    if (forwards) {
-      if (offset >= words[i].length) {
-        ++skip_count;
-        if (skip_count == skip_delay) {
-          forwards = false;
-          skip_count = 0;
-        }
-      }
-    }
-    else {
-      if (offset == 0) {
-        forwards = true;
-        i++;
-        offset = 0;
-        if (i >= len) {
-          i = 0;
-        }
-      }
-    }
-    part = words[i].substring(0, offset);
-    if (skip_count == 0) {
+  var lastFrameTime = null;
+  var update = function (currentTime) {
+    if (!lastFrameTime) lastFrameTime = currentTime;
+    var deltaTime = currentTime - lastFrameTime;
+    if (deltaTime >= speed) {
+      lastFrameTime = currentTime;
       if (forwards) {
-        offset++;
+        if (offset >= words[i].length) {
+          ++skip_count;
+          if (skip_count == skip_delay) {
+            forwards = false;
+            skip_count = 0;
+          }
+        }
       }
       else {
-        offset--;
+        if (offset == 0) {
+          forwards = true;
+          i++;
+          offset = 0;
+          if (i >= len) {
+            i = 0;
+          }
+        }
       }
+      part = words[i].substring(0, offset);
+      if (skip_count == 0) {
+        if (forwards) {
+          offset++;
+        }
+        else {
+          offset--;
+        }
+      }
+      document.querySelector('.word').textContent = part;
     }
-    document.querySelector('.word').textContent = part;
-  }, speed);
+    requestAnimationFrame(update);
+  };
+  requestAnimationFrame(update);
 };
+
+
 
 window.addEventListener('load', function () {
   wordflick();
