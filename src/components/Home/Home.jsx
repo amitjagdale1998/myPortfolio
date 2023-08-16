e5490aa7fd2f5ece010762b7b5c712aa62093628
amitjagdale1998 <amitjagdale1998@gmail.com>
2023-08-16 16:49:36 +0530

    last commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,161 +1,178 @@
-import React from 'react'
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import downloadResume from '../common/downloadResume';
-import './Home.css'
+import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import downloadResume from "../common/downloadResume";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import "./Home.css";
 const Home = () => {
-  function email()
-  {
-    alert
-  ("amitjagdale1998@gmail.com")
+  function email() {
+    alert("amitjagdale1998@gmail.com");
   }
 
-  var words = ['MERNstack Dev','FullStack Dev','Web Dev','Web Designer'];
-var part;
-var i = 0;
-var offset = 0;
-var len = words.length;
-var forwards = true;
-var skip_count = 0;
-var skip_delay = 15;
-var speed = 80;
+  var words = ["MERNstack Dev", "FullStack Dev", "Web Dev", "Web Designer"];
+  var part;
+  var i = 0;
+  var offset = 0;
+  var len = words.length;
+  var forwards = true;
+  var skip_count = 0;
+  var skip_delay = 15;
+  var speed = 80;
 
-// var wordflick = function () {
-//   setInterval(function () {
-//     if (forwards) {
-//       if (offset >= words[i].length) {
-//         ++skip_count;
-//         if (skip_count == skip_delay) {
-//           forwards = false;
-//           skip_count = 0;
-//         }
-//       }
-//     }
-//     else {
-//       if (offset == 0) {
-//         forwards = true;
-//         i++;
-//         offset = 0;
-//         if (i >= len) {
-//           i = 0;
-//         }
-//       }
-//     }
-//     part = words[i].substring(0, offset);
-//     if (skip_count == 0) {
-//       if (forwards) {
-//         offset++;
-//       }
-//       else {
-//         offset--;
-//       }
-//     }
-//     document.querySelector('.word').textContent = part;
-//   }, speed);
-// };
-var words = ['MERNstack Dev','FullStack Dev','Web Dev','Web Designer'];
-var part;
-var i = 0;
-var offset = 0;
-var len = words.length;
-var forwards = true;
-var skip_count = 0;
-var skip_delay = 15;
-var speed = 80;
+  // var wordflick = function () {
+  //   setInterval(function () {
+  //     if (forwards) {
+  //       if (offset >= words[i].length) {
+  //         ++skip_count;
+  //         if (skip_count == skip_delay) {
+  //           forwards = false;
+  //           skip_count = 0;
+  //         }
+  //       }
+  //     }
+  //     else {
+  //       if (offset == 0) {
+  //         forwards = true;
+  //         i++;
+  //         offset = 0;
+  //         if (i >= len) {
+  //           i = 0;
+  //         }
+  //       }
+  //     }
+  //     part = words[i].substring(0, offset);
+  //     if (skip_count == 0) {
+  //       if (forwards) {
+  //         offset++;
+  //       }
+  //       else {
+  //         offset--;
+  //       }
+  //     }
+  //     document.querySelector('.word').textContent = part;
+  //   }, speed);
+  // };
+  var words = ["MERNstack Dev", "FullStack Dev", "Web Dev", "Web Designer"];
+  var part;
+  var i = 0;
+  var offset = 0;
+  var len = words.length;
+  var forwards = true;
+  var skip_count = 0;
+  var skip_delay = 15;
+  var speed = 80;
 
-var wordflick = function () {
-  var lastFrameTime = null;
-  var update = function (currentTime) {
-    if (!lastFrameTime) lastFrameTime = currentTime;
-    var deltaTime = currentTime - lastFrameTime;
-    if (deltaTime >= speed) {
-      lastFrameTime = currentTime;
-      if (forwards) {
-        if (offset >= words[i].length) {
-          ++skip_count;
-          if (skip_count == skip_delay) {
-            forwards = false;
-            skip_count = 0;
-          }
-        }
-      }
-      else {
-        if (offset == 0) {
-          forwards = true;
-          i++;
-          offset = 0;
-          if (i >= len) {
-            i = 0;
-          }
-        }
-      }
-      part = words[i].substring(0, offset);
-      if (skip_count == 0) {
+  var wordflick = function () {
+    var lastFrameTime = null;
+    var update = function (currentTime) {
+      if (!lastFrameTime) lastFrameTime = currentTime;
+      var deltaTime = currentTime - lastFrameTime;
+      if (deltaTime >= speed) {
+        lastFrameTime = currentTime;
         if (forwards) {
-          offset++;
+          if (offset >= words[i].length) {
+            ++skip_count;
+            if (skip_count == skip_delay) {
+              forwards = false;
+              skip_count = 0;
+            }
+          }
+        } else {
+          if (offset == 0) {
+            forwards = true;
+            i++;
+            offset = 0;
+            if (i >= len) {
+              i = 0;
+            }
+          }
         }
-        else {
-          offset--;
+        part = words[i].substring(0, offset);
+        if (skip_count == 0) {
+          if (forwards) {
+            offset++;
+          } else {
+            offset--;
+          }
         }
+        document.querySelector(".word").textContent = part;
       }
-      document.querySelector('.word').textContent = part;
-    }
+      requestAnimationFrame(update);
+    };
     requestAnimationFrame(update);
   };
-  requestAnimationFrame(update);
-};
 
-
-
-window.addEventListener('load', function () {
-  wordflick();
-});
-
-
-
+  window.addEventListener("load", function () {
+    wordflick();
+  });
 
   return (
-    <div className='home-page' >
-           {/* <WordFlick/> */}
-        <div className="container ">
-  <div className="row">
-    <div className="col home-container" id='self-image' align="center">
-    <img className='image'  src='https://www.linkpicture.com/q/student-software-developer-computer-programming-programming-language-computer-software-python-source-code-comment-png-clipart.jpg'></img>
-    </div>
-    <div className="col home-container">
-        <div className='media-icons'>
-        <a href="https://github.com/amitjagdale1998" target='_blank'>
-            <LinkedInIcon/>
-            </a>
-            <a style={{color:'red'}} href='https://www.youtube.com/@prosoftdev/playlists' target='_blank'>
-            <YouTubeIcon/>
-            </a>
-            
-            <FacebookIcon/>
-            
-            <InstagramIcon/>
-        </div>
-    <p style={{fontSize:"2vw"}} className='myname'>Hello,I'M <span style={{
-      color:"orangered",fontFamily:"serif"}}
-      >AMIT</span> </p>
-      <div className="word"></div>
-      
-        <span style={{
-         fontSize:"1.5vw"
-        }} className='myname'>Knack of building 
-        applications with front and back-end operations.</span>
-        <div className='btns' align='center'>
-        <button id='btn1' onClick={email}>Hire me</button>
-        <button id="btn2" onClick={downloadResume}>Resume</button>
-        </div>
-    </div>
-    </div>
-    </div>
-    
-    </div>
-  )
-}
+    <div className="home-page">
+      {/* <WordFlick/> */}
+      <div className="container ">
+        <div className="row mt-5">
+          <div className="col home-container  " id="self-image" align="center">
+            <img
+              className="image"
+              src="https://www.linkpicture.com/q/1686658730159-removebg-preview-1.jpg"
+            ></img>
+          </div>
+          <div className="col home-container">
+            <div className="media-icons">
+              <a href="https://github.com/amitjagdale1998" target="_blank">
+                <GitHubIcon fontSize="large" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/amitjagdale1998"
+                target="_blank"
+              >
+                <LinkedInIcon fontSize="large" />
+              </a>
+              <a
+                style={{ color: "red" }}
+                href="https://www.youtube.com/@prosoftdev/playlists"
+                target="_blank"
+              >
+                <YouTubeIcon fontSize="large" />
+              </a>
 
-export default Home
+              <InstagramIcon fontSize="large" />
+            </div>
+            <p style={{ fontSize: "2vw" }} className="myname">
+              Hello,I'M{" "}
+              <span
+                style={{
+                  color: "orangered",
+                  fontFamily: "serif",
+                }}
+              >
+                AMIT
+              </span>{" "}
+            </p>
+            <div className="word"></div>
+
+            <span
+              style={{
+                fontSize: "1.5vw",
+              }}
+              className="myname"
+            >
+              Knack of building applications with front and back-end operations.
+            </span>
+            <div className="btns mt-3" align="center">
+              <button id="btn1" onClick={email}>
+                Hire me
+              </button>
+              <button id="btn2" onClick={downloadResume}>
+                Resume
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,50 +1,80 @@
-import React from 'react'
-import './about.css'
-import CircleIcon from '@mui/icons-material/Circle';
-import downloadResume from '../common/downloadResume';
+import React from "react";
+import "./about.css";
+import CircleIcon from "@mui/icons-material/Circle";
+import downloadResume from "../common/downloadResume";
 
 function About() {
   return (
-    <div className='about-page' align="center">
-      <h1 className='animate-charcter'>About Me</h1>
-       <h4 >Why Choose Me?</h4>
-       <hr></hr>
-       <div className='shadow-div'>
-        <div className='row'>
-          
-          <div className='col'>
-         <img className='about-image' src='https://www.linkpicture.com/q/web-design-ga5ef6c0aa_1280_1.jpg' height={100} width={200}></img>
-
+    <div className="about-page" align="center">
+      <h1 className="animate-charcter">About Me</h1>
+      <h4>Why Choose Me?</h4>
+      <hr></hr>
+      <div className="shadow-div">
+        <div className="row">
+          <div className="col">
+            <img
+              className="about-image"
+              src="https://www.linkpicture.com/q/web-design-ga5ef6c0aa_1280_1.jpg"
+              height={100}
+              width={200}
+            ></img>
           </div>
-          <div className='col aboutcol2'>
-       <p>Knowledge in MERN stack in building Web Applications,
-        used React for client side,Nodejs/Express for server side addEventListener
-        MongoDB , SQL Server for database. </p>
-        <p>Expertise in using React/Redux to build User interface.
-          Hands-on experience in using React to build Single Page Application and solid 
-          understanding of components and routing ,DoM Manipulation.Knowledge of MongoDB as well as SQL server and Mongoose,.dotenv etc
-        </p>
-       
-       <div className='highlights'>
-       <h4> <label>Here are few Highlights:</label></h4>
-        <span><CircleIcon className='circleicon'/> FullStack web development</span>
-        <span><CircleIcon className='circleicon'/>Interactie design</span>
-        <span><CircleIcon className='circleicon'/>React and BOOtstrap</span>
-        <span><CircleIcon className='circleicon'/>Building REST API</span>
-        <span><CircleIcon className='circleicon'/>Managing SQL and non SQL database</span>
-        </div>
-       
-       <div className='btns-about'>
-        <button id='btn1' onClick={ ()=>alert ("amitjagdale1998@gmail.com")}>Hire me</button>
-        <button id="btn2" onClick={downloadResume}>Get Resume</button>
-        </div>
-       </div>
+          <div className="col aboutcol2">
+            <p>
+              Knowledge in MERN stack in building Web Applications, used React
+              for client side,Nodejs/Express for server side addEventListener
+              MongoDB , SQL Server for database.{" "}
+            </p>
+            <p>
+              Expertise in using React/Redux to build User interface. Hands-on
+              experience in using React to build Single Page Application and
+              solid understanding of components and routing ,DoM
+              Manipulation.Knowledge of MongoDB as well as SQL server and
+              Mongoose,.dotenv etc
+            </p>
 
-       </div>
+            <div className="highlights">
+              <h4>
+                {" "}
+                <label>Here are few Highlights:</label>
+              </h4>
+              <span>
+                <CircleIcon className="circleicon" /> FullStack web development
+              </span>
+              <span>
+                <CircleIcon className="circleicon" />
+                Interactie design
+              </span>
+              <span>
+                <CircleIcon className="circleicon" />
+                React and BOOtstrap
+              </span>
+              <span>
+                <CircleIcon className="circleicon" />
+                Building REST API
+              </span>
+              <span>
+                <CircleIcon className="circleicon" />
+                Managing SQL and non SQL database
+              </span>
+            </div>
 
-</div>
+            <div className="btns-about">
+              <button
+                id="btn1"
+                onClick={() => alert("amitjagdale1998@gmail.com")}
+              >
+                Hire me
+              </button>
+              <button id="btn2" onClick={downloadResume}>
+                Get Resume
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
